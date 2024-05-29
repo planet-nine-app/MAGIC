d401a626efeff32539fdd70f06ed8b72ac6334a8
Lupin <theybetrayedme1@gmail.com>
2024-05-29 14:20:13 +0530

    A minecraft mod implementing MAGIC

---
+++ src/games/minecraft/src/client/java/com/hellbri8nger/magic/ExampleModClient.java
@@ -0,0 +1,10 @@
+package com.hellbri8nger.magic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExampleModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/games/minecraft/src/main/java/com/hellbri8nger/magic/MagicClient.java
@@ -0,0 +1,11 @@
+package com.hellbri8nger.magic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MagicClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+    
+}
